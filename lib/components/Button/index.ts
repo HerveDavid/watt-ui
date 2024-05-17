7ee1ded6fa5b03d75b7007f424f57e7783018d8e
hervedav <david.herve@rte-france.com>
2024-05-17 10:36:54 +0200

    Hot reload

--- lib/components/Button/index.ts
+++ lib/components/Button/index.ts
@@ -1,13 +1,45 @@
-import { LitElement, TemplateResult, html } from "lit";
+import { LitElement, TemplateResult, html, css } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import { buttonStyle } from "./style";
+import { defaultTheme } from "../../styles/defaultTheme";
 
 @customElement('watt-button')
 export default class Button extends LitElement {
 
-    static styles = buttonStyle;
+    static styles = css`
+        button {
+            background-color: ${defaultTheme.colorSurfaceRaised};
+            color: ${defaultTheme.colorAccent};
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
 
-    @property({type: Boolean})
+        button:hover {
+            background-color: ${defaultTheme.colorOverlay}
+        }
+
+        button:focus {
+            background-color: ${defaultTheme.colorSurfaceLowered}
+        }
+
+        button[disabled] {
+            background-color: ${defaultTheme.colorSurfaceLowered};
+            color: ${defaultTheme.colorTextWeakest};
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: not-allowed;
+        } 
+    `;
+
+    @property({ type: Boolean })
     disabled = false;
 
     protected render(): TemplateResult {
