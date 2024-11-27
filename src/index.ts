450b9bae9ce02a831e5cae8ceb14176e49a0b172
HerveDavid <david.herve.code@gmail.com>
2024-11-27 20:44:39 +0100

    readme

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
+// Form Components
+export { WattInput } from "./components/input";
+export { WattFieldset } from "./components/fieldset";
+
+// Navigation Components
+export { WattBreadcrumb, WattBreadcrumbItem } from "./components/breadcrumb";
+export { WattTab } from "./components/tab";
+
+// Interactive Components
 export { WattButton } from "./components/button";
 export {
   WattButtonGroup,
   WattDropdown,
   WattDropdownItem,
 } from "./components/button-group";
-export { WattMessage } from "./components/message";
-export { WattAvatar } from "./components/avatar";
-export { WattInput } from "./components/input";
-export { WattProgressBar } from "./components/progress";
-export { WattTab } from "./components/tab";
-export { WattIcon } from "./components/icon";
-export { WattFieldset } from "./components/fieldset";
 export { WattModal } from "./components/modal";
+
+// Feedback Components
+export { WattMessage } from "./components/message";
+export { WattProgressBar } from "./components/progress";
 export { WattBadge } from "./components/badge";
 export { WattBanner } from "./components/banner";
-export { WattBreadcrumb, WattBreadcrumbItem } from "./components/breadcrumb";
+
+// Visual Components
+export { WattAvatar } from "./components/avatar";
+export { WattIcon } from "./components/icon";
+
+// Layout Components
 export { WattStack } from "./components/stack";
